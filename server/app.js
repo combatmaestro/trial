d2e67f75262c440a843ad8cce471b03819cc8c72
Adarsh Sahu <adarshsahu2510@gmail.com>
2024-05-16 09:11:04 +0530

    ,,

--- server/app.js
+++ server/app.js
@@ -13,7 +13,6 @@ app.use(cors({
   origin: "https://student-iota-livid.vercel.app", 
   credentials: true ,
  }));
- 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileupload());
